f3e45abc595d5333d8609ceb949214073baa8cf2
romulans <austinxaa@gmail.com>
2020-09-15 18:46:15 -0700

    Add missing logic.

--- how_does_the_cowsay.sh
+++ how_does_the_cowsay.sh
@@ -2,15 +2,18 @@
 
 say_this=$@
 cowsay_characters_dir=/usr/share/cowsay/cows/
+
 if [ -d $cowsay_characters_dir ]; then
   cows=$(ls $cowsay_characters_dir)
+else
+  echo Error: Characters not found in $cowsay_characters_dir. Is cowsay installed?
+  exit 1
 fi
 
-# Display help msg and exit if no arguments or --help is specified
+
 if [[ -z $@ || '--help' = $1 ]]; then
   echo Help: Pass in a string for every character in $cowsay_characters_dir to say
   echo This program requires that cowsay be installed and the above directory exist
-
 else
   echo Press Enter to go to the next character
 
